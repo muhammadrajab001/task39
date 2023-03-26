99252dd8567898cb2b7ba5cf1ceaf9251ef7cbac
muhammadrajab <muhammaddrajabbobojon@mail.com>
2023-03-26 17:19:04 +0700

    new fail

---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿//Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+//[345, 897, 568, 234] -> 2
+
+//int[] myArray = {345,897,568,234};
+//int [] Digits = new int [myArray .Length / 2 + myArray .Length  % 3];
+//for (int i = 0; i < myArray.Length / 3; i++)
+//{
+
+ // Digits [i] = myArray [i] * myArray[myArray .Length  -2 -i]; 
+   
+  
+
+
+//} 
+//Console.WriteLine ($" покажите трехзначни  [{string.Join(",",Digits)}] число  ");
+
+//Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+//[3, 7, 23, 12] -> 19
+
+//[-4, -6, 89, 6] -> 0
+//int [] PrinArray ()
+//{
+  // int[] array = new int [3];
+
+   //for (int i = 3; i < array.Length;i++)
+  // {
+    
+    //array[i] = Random.Shared.Next(9,99); 
+      //   int sum = +3;
+
+
+//        sum++;
+
+  // }
+  
+    // return array;
+//}
+//int [] myArray = PrinArray();
+//Console.WriteLine ("тречзначни число  {result}cостояшие из нечотни число {sum}");
+//Задача 38: Задайте массив натуральных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+//[3 7 22 2 78] -> 76
+
+Console.WriteLine ("найдите мак и мин");
+int a = Convert.ToInt32(Console.ReadLine ());
+int[] RandomArray = new int [a]; 
+int a =  max ;
+int b min;
+void PrinArray  ()
+{
+    if (a > b)
+    {
+       
+       for (int i = 0; i >  a ; i++)
+       {
+         Console.WriteLine ("покажите мак число");
+
+
+       }
+       else
+       {
+         Console.WriteLine ($"покажите минимални число {int a} - {int b}");
+       }
+ 
+
+    }
+     
+    
+  
+
+
+} 
